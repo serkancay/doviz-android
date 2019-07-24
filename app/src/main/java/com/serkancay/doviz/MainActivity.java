27432d28f18a4fab3e7861d2202aff770fa34c99
Serkan Çay <sserkancay@gmail.com>
2019-07-24 17:08:16 +0300

    Ana ekrana BottomNavigationBar eklendi

--- app/src/main/java/com/serkancay/doviz/MainActivity.java
+++ app/src/main/java/com/serkancay/doviz/MainActivity.java
@@ -2,18 +2,47 @@ package com.serkancay.doviz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import butterknife.BindView;
+import com.serkancay.doviz.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, MainActivity.class));
     }
 
+    @BindView(R.id.bottomNavigationView)
+    BottomNavigationView bottomNavigationView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    public void bindEvents() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(mNavigationItemSelectedListener);
+    }
+
+    private OnNavigationItemSelectedListener mNavigationItemSelectedListener
+            = new OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull final MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.miExchangeRates: {
+                    return true;
+                }
+                case R.id.miSettings: {
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
 }
